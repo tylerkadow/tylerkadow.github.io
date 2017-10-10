a5af7298ccbe76dd7845beb939beed194063ab5a
Tyler Kadow <tylerkadow@aol.com>
2017-10-10 14:19:45 -0400

    Style changes with jQuery and navigation buttons

---
+++ assets/javascript/profile.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+function checkWidth() {
+    if ($(window).width() < 641){
+        $("#portfolio").removeClass('wrapper').addClass('btn-group').addClass('btn-group-justified');
+        $("#prt").removeClass('wrapper').addClass('btn-group').addClass('btn-group-vertical');
+    }else{
+        $('#portfolio').addClass('wrapper').removeClass('btn-group').removeClass('btn-group-justified');
+        $("#prt").addClass('wrapper').removeClass('btn-group').removeClass('btn-group-vertical');
+}
+
+}
+$(window).ready(checkWidth);
+$(window).resize(checkWidth);
+
+});
