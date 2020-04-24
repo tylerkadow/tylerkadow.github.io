56a46a737279d479961639d54d3e2b2278f9065d
Tyler Kadow <tylerkadow@aol.com>
2020-04-24 18:47:21 -0400

    Add files via upload
    Coin tracker at top of pages

---
+++ assets/js/bit.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2
+  })
+
+  const dogeFormatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 6
+  })
+
+// Load cryptocurrency prices
+  $.ajax({
+    url: "https://api.coinbase.com/v2/prices/spot?currency=USD",
+    type: "GET",
+    dataType: 'json',
+  })
+  .done(function(data){
+    $('#btc-price').append(
+      "<span><b>" + formatter.format(data.data.amount) + "</span> ");
+  });
+
+  $.ajax({
+    url: "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=209e1a11-8ebc-4fc2-954c-eccc65c94c1e",
+    type: "GET",
+    dataType: 'json'
+  })
+  .done(function(data){
+    console.log(data);
+    $('#doge-price').append(
+      "<span><b>" + dogeFormatter.format(data.data[31].quote.USD.price) + "</span> ");
+    $('#eth-price').append(
+      "<span><b>" + formatter.format(data.data[1].quote.USD.price) + "</span> ");
+    $('#xmr-price').append(
+      "<span><b>" + formatter.format(data.data[13].quote.USD.price) + "</span> ");
+    $('#xrp-price').append(
+      "<span><b>" + formatter.format(data.data[2].quote.USD.price) + "</span> ");
+  });
+
+  //209e1a11-8ebc-4fc2-954c-eccc65c94c1e
+});
