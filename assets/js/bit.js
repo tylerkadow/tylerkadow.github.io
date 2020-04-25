1be744ada9fb86529c229b61e1d9d36a2e23fa12
Tyler Kadow <tylerkadow@aol.com>
2020-04-24 21:24:21 -0400

    Add files via upload

--- assets/js/bit.js
+++ assets/js/bit.js
@@ -29,10 +29,8 @@ $(document).ready(function(){
     dataType: 'json'
   })
   .done(function(data){
-    if (data !== undefined){
     $('#alt-price').append(
       "<span> • Dogecoin: <b>" + dogeFormatter.format(data.data[31].quote.USD.price) + "</b></span><span>  • Ethereum: <b>" + formatter.format(data.data[1].quote.USD.price) + "</b></span><span>  • Monero: <b>" + formatter.format(data.data[13].quote.USD.price) + "</b></span><span> • Ripple: <b>" + formatter.format(data.data[2].quote.USD.price) + "</b></span> ");
-    }
   });
 
   //209e1a11-8ebc-4fc2-954c-eccc65c94c1e
